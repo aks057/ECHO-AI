bc034a47890d1c88190b744a6dab4ab4eed9ad2d
Abhinash Kumar <abhinash.iiitl@gmail.com>
2024-05-13 14:51:56 +0530

    updated folder structure

---
+++ backend/src/index.ts
@@ -0,0 +1,16 @@
+import { connect } from "http2";
+import app from "./app.js"
+import {connectToDatabase} from "./db/connection.js";
+
+//connections and listners
+const PORT =process.env.PORT || 5000;
+connectToDatabase()
+    .then(() => {
+        app.listen(PORT, () => console.log("Server Open and database connected"));
+    })
+    .catch((err) => console.log(err));
+    
+
+
+
+
