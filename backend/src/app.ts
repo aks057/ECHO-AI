97008eb74a2ce3097ae99348d40038d542cc8ad8
Abhinash Kumar <abhinash.iiitl@gmail.com>
2024-05-14 08:17:43 +0530

    initial frontend and backend

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,6 +2,8 @@ import express, { Router } from "express";
 import {config} from "dotenv";
 import morgan from "morgan";
 import appRouter from "./routes/index.js";
+import { cookie } from "express-validator";
+import cookieParser from "cookie-parser";
 
 config();
 
@@ -9,6 +11,8 @@ const app = express();
 
 //middlewares
 app.use(express.json());
+app.use(cookieParser(process.env.COOKIE_SECRET));
+
 //remove it in production 
 app.use(morgan("dev"));
 app.use("/api/v1", appRouter);
