af497cd7290e1d26b086c11096742b5563c4e102
Abhinash Kumar <abhinash.iiitl@gmail.com>
2024-05-16 13:33:36 +0530

    login page

--- backend/dist/controllers/user-controllers.js
+++ backend/dist/controllers/user-controllers.js
@@ -37,7 +37,9 @@ export const userSignup = async (req, res, next) => {
             httpOnly: true,
             signed: true,
         });
-        return res.status(201).json({ message: "OK", id: user._id.toString() });
+        return res
+            .status(201)
+            .json({ message: "OK", name: user.name, email: user.email });
     }
     catch (error) {
         console.log(error);
@@ -72,7 +74,9 @@ export const userLogin = async (req, res, next) => {
             httpOnly: true,
             signed: true,
         });
-        return res.status(201).json({ message: "OK", id: user._id.toString() });
+        return res
+            .status(200)
+            .json({ message: "OK", name: user.name, email: user.email });
     }
     catch (error) {
         console.log(error);
